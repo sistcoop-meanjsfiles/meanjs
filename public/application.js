2c48f125106a8f326320bad409384819f0e07d7d
Carlos <carlosthe19916@gmail.com>
2015-06-05 18:12:25 -0500

    add keycloak bearer config

--- public/application.js
+++ public/application.js
@@ -92,3 +92,127 @@ angular.element(document).ready(function() {
 	});
 
 });
+
+
+
+angular.module('mean').controller('KeycloakController',
+	function ($scope, Auth) {
+
+		$scope.logout = function(){
+			Auth.authz.logout();
+		};
+		$scope.accountManagement = function(){
+			Auth.authz.accountManagement();
+		};
+
+		$scope.user = {
+			username: Auth.authz.idTokenParsed.preferred_username,
+			roles: []
+		};
+
+		$scope.loadRoles = function () {
+			var realmRoles = Auth.authz.realmAccess.roles;
+			for (var i = 0; i < realmRoles.length; i++) {
+				$scope.user.roles.push(realmRoles[i]);
+			}
+		};
+		$scope.loadRoles();
+
+	}
+);
+
+var resourceRequests = 0;
+var loadingTimer = -1;
+
+angular.module('mean').factory('authInterceptor', function ($q, Auth) {
+	return {
+		request: function (config) {
+			if (!config.url.match(/.html$/)) {
+				var deferred = $q.defer();
+				if (Auth.authz.token) {
+					Auth.authz.updateToken(5).success(function () {
+						config.headers = config.headers || {};
+						config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+						deferred.resolve(config);
+					}).error(function () {
+						location.reload();
+					});
+				}
+				return deferred.promise;
+			} else {
+				return config;
+			}
+		}
+	};
+});
+
+angular.module('mean').factory('spinnerInterceptor', function ($q, $window, $rootScope, $location) {
+	return function (promise) {
+		return promise.then(function (response) {
+			resourceRequests--;
+			if (resourceRequests == 0) {
+				if (loadingTimer != -1) {
+					window.clearTimeout(loadingTimer);
+					loadingTimer = -1;
+				}
+				$('#loading').hide();
+			}
+			return response;
+		}, function (response) {
+			resourceRequests--;
+			if (resourceRequests == 0) {
+				if (loadingTimer != -1) {
+					window.clearTimeout(loadingTimer);
+					loadingTimer = -1;
+				}
+				$('#loading').hide();
+			}
+
+			return $q.reject(response);
+		});
+	};
+});
+
+angular.module('mean').factory('errorInterceptor', function ($q, $window, $rootScope, $location, Auth) {
+	return function (promise) {
+		return promise.then(function (response) {
+			return response;
+		}, function (response) {
+			if (response.status == 401) {
+				Auth.authz.logout();
+			} else if (response.status == 403) {
+				$location.path('/forbidden');
+			} else if (response.status == 404) {
+				$location.path('/notfound');
+			} else if (response.status) {
+				if (response.data && response.data.errorMessage) {
+					alert(response.data.errorMessage);
+				} else {
+					alert.error("An unexpected server error has occurred");
+				}
+			}
+			return $q.reject(response);
+		});
+	};
+});
+
+angular.module('mean').config(function ($httpProvider) {
+	$httpProvider.interceptors.push('errorInterceptor');
+
+	var spinnerFunction = function (data, headersGetter) {
+		if (resourceRequests == 0) {
+			loadingTimer = window.setTimeout(function () {
+				$('#loading').show();
+				loadingTimer = -1;
+			}, 500);
+		}
+		resourceRequests++;
+		return data;
+	};
+	$httpProvider.defaults.transformRequest.push(spinnerFunction);
+
+	$httpProvider.interceptors.push('spinnerInterceptor');
+	$httpProvider.interceptors.push('authInterceptor');
+
+});
