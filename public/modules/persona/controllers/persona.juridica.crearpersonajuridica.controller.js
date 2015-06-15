700343d16ee03871f3294e8d789f12ee63b808fd
Carlos <carlosthe19916@gmail.com>
2015-06-15 18:26:17 -0500

    persona juridica

--- public/modules/persona/controllers/persona.juridica.crearpersonajuridica.controller.js
+++ public/modules/persona/controllers/persona.juridica.crearpersonajuridica.controller.js
@@ -8,81 +8,32 @@ angular.module('persona').controller('Persona.Juridica.CrearPersonaJuridicaContr
 			persona: SGPersonaJuridica.$build()
 		};
 
-		$scope.loadParams = function () {
-			$scope.view.persona.tipoDocumento = $stateParams.tipoDocumento;
-			$scope.view.persona.numeroDocumento = $stateParams.numeroDocumento;
-		};
-		$scope.loadParams();
-
-		$scope.loadDefaultConfiguration = function () {
-			$scope.view.persona.codigoPais = 'PER';
-		};
-		$scope.loadDefaultConfiguration();
-
-		$scope.combo = {
-			pais: SGCountryCode.$search().$object,
-			tipoDocumento: SGTipoDocumento.$search({tipoPersona: 'juridica'}).$object,
-			tipoEmpresa: SGTipoEmpresa.$search().$object
-		};
-		$scope.combo.selected = {
-			pais: undefined,
-			tipoDocumento: undefined,
-			tipoEmpresa: undefined
-		};
-
-		$scope.check = function ($event) {
-			if (!angular.isUndefined($event))
-				$event.preventDefault();
-			if (!angular.isUndefined($scope.combo.selected.tipoDocumento) && !angular.isUndefined($scope.view.persona.numeroDocumento)) {
-				SGPersonaJuridica.$findByTipoNumeroDocumento($scope.combo.selected.tipoDocumento.abreviatura, $scope.view.persona.numeroDocumento).then(function (data) {
-					if (!data)
-						toastr.info('Documento de identidad disponible', 'Info');
-					else
-						toastr.warn('Documento de identidad no disponible', 'Warning');
-				});
-			}
-		};
-
-
-		$scope.goTabRepresentante = function () {
-			if ($scope.form.$valid) {
-				$state.go('^.representante');
-			} else {
-				$scope.form.$setSubmitted();
-			}
-		};
-
-
-		$scope.submit = function () {
-			if ($scope.form.$valid) {
-
-				if (angular.isUndefined($scope.view.persona.representanteLegal)) {
-					toastr.warning('Representante legal no definido.', 'Warning');
-					return;
-				}
-				if (angular.isUndefined($scope.view.persona.representanteLegal.id)) {
-					toastr.warning('Representante legal no definido.', 'Warning');
-					return;
-				}
-
-				SGPersonaJuridica.$findByTipoNumeroDocumento($scope.view.persona.tipoDocumento, $scope.view.persona.numeroDocumento).then(function (response) {
-					if (response) {
-						toastr.error('Documento de identidad no disponible');
-					} else {
-						$scope.save();
-					}
-				});
-			}
-		};
-
 		$scope.save = function () {
+
+			if (angular.isUndefined($scope.view.persona.representanteLegal)) {
+				toastr.warning('Representante legal no definido.', 'Warning');
+				return;
+			}
+			if (angular.isUndefined($scope.view.persona.representanteLegal.id)) {
+				toastr.warning('Representante legal no definido.', 'Warning');
+				return;
+			}
+
+			SGPersonaJuridica.$findByTipoNumeroDocumento($scope.view.persona.tipoDocumento, $scope.view.persona.numeroDocumento).then(function (response) {
+				if (response) {
+					toastr.error('Documento de identidad no disponible');
+				} else {
+					$scope.save();
+				}
+			});
+
 			$scope.view.persona.representanteLegal = {
 				tipoDocumento: $scope.view.persona.representanteLegal.tipoDocumento,
 				numeroDocumento: $scope.view.persona.representanteLegal.numeroDocumento
 			};
 			$scope.view.persona.$save().then(
 				function (response) {
-					toastr.success('Persona creada', 'Info');
+					toastr.success('Persona creada');
 					$state.go('^.^.editar', {personaJuridica: response.id});
 				},
 				function error(err) {
--- public/modules/persona/controllers/persona.juridica.crearpersonajuridica.datosprincipales.controller.js
+++ public/modules/persona/controllers/persona.juridica.crearpersonajuridica.datosprincipales.controller.js
@@ -2,6 +2,50 @@
 
 /* jshint -W098 */
 angular.module('persona').controller('Persona.Juridica.CrearPersonaJuridica.DatosPrincipalesController',
-	function ($scope) {
+	function ($scope, SGCountryCode, SGTipoDocumento, SGTipoEmpresa) {
+
+		$scope.combo = {
+			pais: SGCountryCode.$search().$object,
+			tipoDocumento: SGTipoDocumento.$search({tipoPersona: 'juridica'}).$object,
+			tipoEmpresa: SGTipoEmpresa.$search().$object
+		};
+		$scope.combo.selected = {
+			pais: undefined,
+			tipoDocumento: undefined,
+			tipoEmpresa: undefined
+		};
+
+		$scope.loadParams = function () {
+			$scope.view.persona.tipoDocumento = $stateParams.tipoDocumento;
+			$scope.view.persona.numeroDocumento = $stateParams.numeroDocumento;
+		};
+		$scope.loadParams();
+
+		$scope.loadDefaultConfiguration = function () {
+			$scope.view.persona.codigoPais = 'PER';
+		};
+		$scope.loadDefaultConfiguration();
+
+		$scope.check = function ($event) {
+			if (!angular.isUndefined($event))
+				$event.preventDefault();
+			if (!angular.isUndefined($scope.combo.selected.tipoDocumento) && !angular.isUndefined($scope.view.persona.numeroDocumento)) {
+				SGPersonaJuridica.$findByTipoNumeroDocumento($scope.combo.selected.tipoDocumento.abreviatura, $scope.view.persona.numeroDocumento).then(function (data) {
+					if (!data)
+						toastr.info('Documento de identidad disponible', 'Info');
+					else
+						toastr.warn('Documento de identidad no disponible', 'Warning');
+				});
+			}
+		};
+
+		$scope.goTabRepresentante = function () {
+			if ($scope.form.$valid) {
+				$state.go('^.representante');
+			} else {
+				$scope.form.$setSubmitted();
+			}
+		};
+
 
 	});
