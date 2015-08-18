2c8338f64bb8c590152766751949e8007d430e8c
Carlos <carlosthe19916@gmail.com>
2015-08-18 18:41:07 -0500

    persona crear

--- modules/persona/client/controllers/persona/natural/crear.client.controller.js
+++ modules/persona/client/controllers/persona/natural/crear.client.controller.js
@@ -2,68 +2,90 @@
 
 /* jshint -W098 */
 angular.module('persona').controller('Persona.Natural.CrearPersonaNaturalController',
-	function ($scope, $state, $stateParams, toastr, SGCountryCode, SGTipoDocumento, SGEstadoCivil, SGSexo, SGPersonaNatural) {
+    function ($scope, $state, $stateParams, toastr, SGCountryCode, SGTipoDocumento, SGEstadoCivil, SGSexo, SGPersonaNatural) {
 
-		$scope.view = {
-			persona: SGPersonaNatural.$build()
-		};
+        $scope.working = false;
 
-		$scope.combo = {
-			pais: SGCountryCode.$search().$object,
-			tipoDocumento: SGTipoDocumento.$search({tipoPersona: 'natural'}).$object,
-			sexo: SGSexo.$search().$object,
-			estadoCivil: SGEstadoCivil.$search().$object
-		};
-		$scope.combo.selected = {
-			pais: undefined,
-			tipoDocumento: undefined,
-			sexo: undefined,
-			estadoCivil: undefined
-		};
+        $scope.view = {
+            persona: SGPersonaNatural.$build()
+        };
 
-		$scope.loadParams = function () {
-			$scope.view.persona.tipoDocumento = $stateParams.tipoDocumento;
-			$scope.view.persona.numeroDocumento = $stateParams.numeroDocumento;
-		};
-		$scope.loadParams();
+        $scope.combo = {
+            pais: undefined,
+            tipoDocumento: undefined,
+            sexo: undefined,
+            estadoCivil: undefined
+        };
+        $scope.combo.selected = {
+            pais: undefined,
+            tipoDocumento: undefined,
+            sexo: undefined,
+            estadoCivil: undefined
+        };
 
-		$scope.loadDefaultConfiguration = function () {
-			$scope.view.persona.codigoPais = 'PER';
-		};
-		$scope.loadDefaultConfiguration();
+        $scope.loadCombos = function(){
+            SGCountryCode.$search().then(function(response){
+                $scope.combo.pais = response.items;
+            });
+            SGTipoDocumento.$search({tipoPersona: 'natural'}).then(function(response){
+                $scope.combo.tipoDocumento = response.items;
+            });
+            SGSexo.$search().then(function(response){
+                $scope.combo.sexo = response.items;
+            });
+            SGEstadoCivil.$search().then(function(response){
+                $scope.combo.estadoCivil = response.items;
+            });
+        };
+        $scope.loadCombos();
 
-		$scope.check = function ($event) {
-			if (!angular.isUndefined($event))
-				$event.preventDefault();
-			if (!angular.isUndefined($scope.combo.selected.tipoDocumento) && !angular.isUndefined($scope.view.persona.numeroDocumento)) {
-				SGPersonaNatural.$findByTipoNumeroDocumento($scope.combo.selected.tipoDocumento.abreviatura, $scope.view.persona.numeroDocumento).then(function (data) {
-					if (!data)
-						toastr.info('Documento de identidad disponible');
-					else
-						toastr.warning('Documento de identidad no disponible');
-				});
-			}
-		};
+        $scope.loadParams = function () {
+            $scope.view.persona.tipoDocumento = $stateParams.tipoDocumento;
+            $scope.view.persona.numeroDocumento = $stateParams.numeroDocumento;
+        };
+        $scope.loadParams();
 
-		$scope.save = function () {
-			var save = function () {
-				$scope.view.persona.$save().then(
-					function (response) {
-						toastr.success('Persona creada');
-						$state.go('^.editar', {personaNatural: response.id});
-					},
-					function error(err) {
-						toastr.error(err.data.message);
-					}
-				);
-			};
-			SGPersonaNatural.$findByTipoNumeroDocumento($scope.view.persona.tipoDocumento, $scope.view.persona.numeroDocumento).then(function (data) {
-				if (data) {
-					toastr.error('Documento de identidad no disponible', 'Error');
-				} else {
-					save();
-				}
-			});
-		};
+        $scope.loadDefaultConfiguration = function () {
+            $scope.view.persona.codigoPais = 'PER';
+        };
+        $scope.loadDefaultConfiguration();
 
-	});
+        $scope.check = function ($event) {
+            if (!angular.isUndefined($event))
+                $event.preventDefault();
+            if (!angular.isUndefined($scope.combo.selected.tipoDocumento) && !angular.isUndefined($scope.view.persona.numeroDocumento)) {
+                SGPersonaNatural.$search({documento:$scope.combo.selected.tipoDocumento.abreviatura, numero:$scope.view.persona.numeroDocumento}).then(function (response) {
+                    if (!response.items.length)
+                        toastr.info('Documento de identidad disponible');
+                    else
+                        toastr.warning('Documento de identidad no disponible');
+                });
+            }
+        };
+
+        $scope.save = function () {
+            var save = function () {
+                $scope.view.persona.sexo = $scope.combo.selected.sexo;
+                $scope.view.persona.estadoCivil = $scope.combo.selected.estadoCivil;
+                $scope.working = true;
+                $scope.view.persona.$save().then(
+                    function (response) {
+                        toastr.success('Persona creada');
+                        $scope.working = false;
+                        $state.go('^.editar', {personaNatural: response.id});
+                    },
+                    function error(err) {
+                        toastr.error(err.data.message);
+                    }
+                );
+            };
+            SGPersonaNatural.$search({documento:$scope.view.persona.tipoDocumento, numero:$scope.view.persona.numeroDocumento}).then(function (response) {
+                if (response.items.length) {
+                    toastr.error('Documento de identidad no disponible', 'Error');
+                } else {
+                    save();
+                }
+            });
+        };
+
+    });
