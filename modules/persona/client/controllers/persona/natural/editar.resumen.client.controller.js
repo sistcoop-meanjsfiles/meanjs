e9f7e5b531c89239e6167b0efd19024603f87722
carlos <carlosthe19916@gmail.com>
2015-09-19 00:14:13 -0500

    persona natural resumen

--- modules/persona/client/controllers/persona/natural/editar.resumen.client.controller.js
+++ modules/persona/client/controllers/persona/natural/editar.resumen.client.controller.js
@@ -2,31 +2,31 @@
 
 /* jshint -W098 */
 angular.module('persona').controller('Persona.Natural.EditarPersonaNatural.ResumenController',
-	function ($scope, $modal, personaNatural) {
+    function ($scope, $modal, personaNatural) {
 
-		$scope.view = {
-			persona: personaNatural
-		};
+        $scope.view = {
+            persona: personaNatural
+        };
 
-		$scope.openCambiarFotoModal = function () {
+        $scope.openCambiarFotoModal = function () {
 
-			var modalInstance = $modal.open({
-				animation: true,
-				templateUrl: '/modules/persona/client/views/natural/form-editar-cambiarFoto.html',
-				controller: 'Persona.Natural.EditarPersonaNatural.CambiarFotoModalController',
-				size: 'lg',
-				resolve: {
-					personaNatural: function () {
-						return $scope.view.persona;
-					}
-				}
-			});
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: '/modules/persona/client/views/natural/form-editar-cambiarFoto.html',
+                controller: 'Persona.Natural.EditarPersonaNatural.CambiarFotoModalController',
+                size: 'lg',
+                resolve: {
+                    personaNatural: function () {
+                        return $scope.view.persona;
+                    }
+                }
+            });
 
-			modalInstance.result.then(function (persona) {
-				$scope.view.persona = persona;
-			}, function () {
-			});
+            modalInstance.result.then(function (persona) {
+                $scope.view.persona = persona;
+            }, function () {
+            });
 
-		};
+        };
 
-	});
+    });
